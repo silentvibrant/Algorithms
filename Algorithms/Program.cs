e41cd6eb211c68fe5300d3df8470492253339b7a
silentvibrant <silentvibrant@gmail.com>
2019-05-06 02:12:02 +0100

    binary search algorithm added

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
-namespace Algorithms
+namespace BinarySearch
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var intArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Please choose a number between 1 and 10");
+
+            int answer = int.Parse(Console.ReadLine());
+            int? position = BinarySearch(intArray, answer);
+
+            Console.WriteLine($"Your position is {position}");
+            Console.ReadLine();
+        }
+
+        public static int? BinarySearch(int[] sortedIntArray, int item)
+        {
+            int lowNumber = 0;
+            int highNumber = sortedIntArray.Length - 1;
+
+            while (lowNumber <= highNumber)
+            {
+                int middleNumber = (lowNumber + highNumber) / 2;
+                int guess = sortedIntArray[middleNumber];
+                if (guess == item)
+                    return middleNumber;
+                if (guess < item)
+                    lowNumber = middleNumber + 1;
+                else
+                    highNumber = middleNumber - 1;
+            }
+
+            return null;
         }
     }
 }
