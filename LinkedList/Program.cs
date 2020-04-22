e2f77c7fd261ec8acf0cfdecc58a2f0a4fbd8e3f
SilentVibrant <silentvibrant@gmail.com>
2020-04-22 08:44:32 +0100

    Linked list work

---
+++ LinkedList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = new Node {Value = 3};
+            var second = new Node { Value = 5 };
+            var third = new Node { Value = 7 };
+
+            first.Next = second;
+            second.Next = third;
+
+            PrintList(first);
+        }
+
+        public static void PrintList(Node node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+}
